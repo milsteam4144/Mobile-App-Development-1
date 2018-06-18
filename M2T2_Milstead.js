03729f0666eaa504119a8bce6f82eb9dce041c4e
Mallory Milstead <milsteam4144@student.faytechcc.edu>
2018-06-18 10:37:41 -0400

    Add files via upload

---
+++ M2T2_Milstead.js
@@ -0,0 +1,153 @@
+//INTRODUCTION
+//Console
+//Question 1
+console.log("Mushrooms!");
+
+//Question 2
+console.log("Making a good brain great");
+
+
+//Data Types
+//Question 1
+console.log("JavaScript");
+
+//Question 2
+console.log(33.7);
+
+//Question 3
+console.log(true);
+
+//Question 4
+console.log(null);
+
+
+//Math Operators
+//Question 1
+console.log(29 + 3.5);
+
+//Question 2
+console.log(2018 - 1969);
+
+//Question 3
+console.log(65/240);
+
+//Question 4
+console.log(0.2708 * 100);
+
+
+//Properties
+//Question 1
+console.log("Teaching the world how to code".length);
+
+
+//Built-in Methods
+//Question 1
+console.log('Codecademy'.toUpperCase()); 
+
+//Question 2
+console.log('    Remove whitespace   '.trim());
+
+//Libraries
+//Question 1
+console.log(Math.random()*100);
+
+//Question 2
+console.log(Math.floor(Math.random()*100));
+
+//Question 3
+console.log(Math.ceil(43.8));
+
+//Question 4
+console.log(Number.isInteger(2017));
+
+
+//Comments
+//Question 1
+//Opening line
+
+//Question 2
+/*console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');*/
+
+
+//Review Types and Operators:No Code
+
+
+//VARIABLES
+//Question 1:No Code
+
+//Question 2
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+
+//Create a Variable: const
+//Question 1
+const entree = "Enchiladas";
+
+//Question 2
+const price = 12;
+
+//Question 3
+console.log(entree);
+console.log(price);
+
+//Question 4
+entree = 'Tacos';
+
+
+//Create a Variable: let
+//Question 1
+let changeMe = true;
+
+//Question 2
+changeMe = false;
+console.log(changeMe);
+
+
+//Undefined
+//Question 1
+let notDefined;
+console.log(notDefined);
+
+//Question 2
+let valueless;
+console.log(valueless);
+
+
+//Mathematical Assignment Operators
+//Question 1
+molecule +=16;
+
+//Question 2
+particle *=6.02;
+
+//Question 3
+assay++;
+
+
+//String Interpolation
+//Question 1
+let favoriteAnimal = "Pit Bull";
+
+//Question 2
+console.log("My favorite animal: " + favoriteAnimal);
+
+
+//String Interpolation II
+//Question 1
+const myName = "Mallory";
+
+//Question 2
+let myCity = "Denver";
+
+//Question 3
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+
+//Review Variables:No Code
